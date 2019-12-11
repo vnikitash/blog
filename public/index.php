4622230161c84f548ed204381ffc4fec7ff12b9c
Viktor <viktor@stryber.com>
2019-12-11 18:18:43 +0200

    NVV | Test

--- public/index.php
+++ public/index.php
@@ -22,11 +22,6 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-
-
-
-
-
 require __DIR__.'/../vendor/autoload.php';
 /*
 |--------------------------------------------------------------------------
